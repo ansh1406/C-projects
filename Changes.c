b5d4c703b4e71df01896b620827de20ff4ac5d3c
ansh1406 <122546794+ansh1406@users.noreply.github.com>
2024-10-08 17:58:44 +0530

    Update Changes.c
    Aise hi timepass

--- Changes.c
+++ Changes.c
@@ -19,52 +19,66 @@ int main(void)
         printf("Change owed: ");
         scanf("%d", &rupees);
     } while (rupees < 0);
+    int fiveH = 0,twoH=0,oneH=0,fifty=0,twenty=0,ten=0,five=0,two=0,one=0;
 
     // Getting and printing different value currency by calling function and updating rupees value after each
 
-    int fivehundred = calculate_fivehundred(rupees);
-    printf("Number of fivehundred: %d\n", fivehundred);
-    rupees = rupees - (fivehundred * 500);
+    while(rupees>0){
+       if(rupees>=500) {rupees-=500; fiveH++; continue;}
+       if(rupees>=200) {rupees-=200; twoH++; continue;}
+        if(rupees>=100) {rupees-=100; oneH++; continue;}
+        if(rupees>=50) {rupees-=50; fifty++; continue;}
+       if(rupees>=20) {rupees-=20; twenty++; continue;}
+        if(rupees>=10) {rupees-=10; ten++; continue;}
+        if(rupees>=5) {rupees-=5; five++; continue;}
+       if(rupees>=2) {rupees-=2; two++; continue;}
+        if(rupees>=1) {rupees-=1; one++; continue;}
+    }
 
-    int twohundred = calculate_twohundred(rupees);
-    printf("Number of twohundred: %d\n", twohundred);
-    rupees = rupees - (twohundred * 200);
 
-    int onehundred = calculate_onehundred(rupees);
-    printf("Number of onehundred: %d\n", onehundred);
-    rupees = rupees - (onehundred * 100);
+    //int fivehundred = calculate_fivehundred(rupees);
+    printf("Number of fivehundred: %d\n", fiveH);
+   // rupees = rupees - (fivehundred * 500);
 
-    int fifty = calculate_fifty(rupees);
+ //   int twohundred = calculate_twohundred(rupees);
+    printf("Number of twohundred: %d\n", twoH);
+   // rupees = rupees - (twohundred * 200);
+
+   // int onehundred = calculate_onehundred(rupees);
+    printf("Number of onehundred: %d\n", oneH);
+   // rupees = rupees - (onehundred * 100);
+
+   // int fifty = calculate_fifty(rupees);
     printf("Number of fifty: %d\n", fifty);
-    rupees = rupees - (fifty * 50);
+    //rupees = rupees - (fifty * 50);
 
-    int twenty = calculate_twenty(rupees);
+   // int twenty = calculate_twenty(rupees);
     printf("Number of twenty: %d\n", twenty);
-    rupees = rupees - (twenty * 20);
+   // rupees = rupees - (twenty * 20);
 
-    int ten = calculate_ten(rupees);
+  //  int ten = calculate_ten(rupees);
     printf("Number of ten: %d\n", ten);
-    rupees = rupees - (ten * 10);
+    //rupees = rupees - (ten * 10);
 
-    int five = calculate_five(rupees);
+  //  int five = calculate_five(rupees);
     printf("Number of five: %d\n", five);
-    rupees = rupees - (five * 5);
+   // rupees = rupees - (five * 5);
 
-    int two = calculate_two(rupees);
+   // int two = calculate_two(rupees);
     printf("Number of two: %d\n", two);
-    rupees = rupees - (two * 2);
+    //rupees = rupees - (two * 2);
 
-    int one = calculate_one(rupees);
+   // int one = calculate_one(rupees);
     printf("Number of one: %d\n", one);
-    rupees = rupees - (one * 1);
+  //  rupees = rupees - (one * 1);
     
     // Printing total notes/coins to be given
-    printf("Total: %d\n", fivehundred + twohundred +onehundred + fifty + twenty + ten + five + two + one);
+    printf("Total: %d\n", fiveH + twoH +oneH+ fifty + twenty + ten + five + two + one);
 }
 
 // Different functions to calculate different currencies
 
-int calculate_fivehundred(int rupees)
+/*int calculate_fivehundred(int rupees)
 {
     int fivehundred = 0;
     while (rupees >= 500)
@@ -161,4 +175,4 @@ int calculate_one(int rupees)
         rupees = rupees - 1;
     }
     return one;
-}
+}*/
